ab2c4bd0c1fd20c88583054eaaa35542b01c444b
Zafindramoma Mitondratsara <mitondratsaramuriellaz@gmail.com>
2022-09-04 17:17:18 +0200

    Classe TestJeux

---
+++ TestJeux.py
@@ -0,0 +1,10 @@
+import print as print
+
+from Jeux import Jeux
+from CodeRayon import CodeRayon
+
+if __name__ == '__main__':
+    code_rayon = CodeRayon("FRA.12.42.2021")
+    jeu = Jeux("console", "sport", 200, "cool description", code_rayon)
+
+    print(jeu.jeu_is_valide())
