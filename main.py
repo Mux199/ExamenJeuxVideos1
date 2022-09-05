8543e255f887f5faffed4806fa3736472ab240d7
Zafindramoma Mitondratsara <mitondratsaramuriellaz@gmail.com>
2022-09-05 21:36:12 +0200

    Revue des classes Code Rayon et Jeux Faire les tests unitaires

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from Jeux import Jeux
 from CodeRayon import CodeRayon
 
+
 if __name__ == '__main__':
     code_rayon = CodeRayon("FRA.12.42.2021")
     jeu = Jeux("console", "sport", 200, "cool description", code_rayon)
 
-    print(jeu.jeu_is_valide())
+    print(jeu.is_jeu_valide())
+
